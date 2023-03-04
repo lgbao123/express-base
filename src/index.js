c58b74bdc0344d8003113d9d288bd69c57ad8169
lgbao123 <luugiabao2609@gmail.com>
2023-03-04 16:23:48 +0700

    project structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 const express = require('express')
 const path = require('path')
 require('dotenv').config()
-
+const configViewEngine = require('./config/viewEngine')
+const webRouters = require('./routes/web')
 
 const app = express()
 const port = process.env.PORT || 8000
+const hostname = process.env.hostname
 
-// config template 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, 'view'));
-//config static file 
-app.use(express.static(path.join(__dirname, 'public')))
+// config template - config static file 
+configViewEngine(app)
 
 // route
-app.get('/', (req, res) => {
-   res.render('index')
-})
+app.use('/v1', webRouters);
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
    console.log(`Example app listening on port :${port}`)
 })
 
-console.log(path.join(__dirname));
